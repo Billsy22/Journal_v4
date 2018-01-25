980ab8b52d1f529b05fac72778b48e45905aabb6
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-01-25 14:48:12 -0700

    Set up model: Entry with properties, initializer, and conforms to the equatable protocol

---
+++ Journal_v4/Models/Entry.swift
@@ -0,0 +1,31 @@
+//
+//  Entry.swift
+//  Journal_v4
+//
+//  Created by Taylor Bills on 1/25/18.
+//  Copyright © 2018 Taylor Bills. All rights reserved.
+//
+
+import Foundation
+
+class Entry: Equatable, Codable {
+    
+    // MARK:    Properties
+    
+    var title: String
+    var body: String
+    var timeStamp = Date()
+    
+    // MARK:    Initializer
+    init(title: String, body: String) {
+        self.title = title
+        self.body = body
+    }
+    
+    // MARK:    Equatable Protocol
+    static func == (lhs: Entry, rhs: Entry) -> Bool {
+        return lhs.title == rhs.title &&
+        lhs.body == rhs.body &&
+        lhs.timeStamp == rhs.timeStamp
+    }
+}
